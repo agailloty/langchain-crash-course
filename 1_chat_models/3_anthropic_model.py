8116259ead4ffd07b0fdcf888c49269d8f6a038a
Axel-Cleris Gailloty <mr.clerisaxel@hotmail.fr>
2025-02-14 21:39:46 +0100

    add ollama support

---
+++ 1_chat_models/3_anthropic_model.py
@@ -0,0 +1,18 @@
+from langchain_anthropic import ChatAnthropic
+from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage, SystemMessage
+
+# Setup environment variables and messages
+load_dotenv()
+
+messages = [
+    SystemMessage(content="Solve the following math problems"),
+    HumanMessage(content="What is 81 divided by 9?"),
+]
+
+# Create a Anthropic model
+# Anthropic models: https://docs.anthropic.com/en/docs/models-overview
+model = ChatAnthropic(model="claude-3-opus-20240229")
+
+result = model.invoke(messages)
+print(f"Answer from Anthropic: {result.content}")
